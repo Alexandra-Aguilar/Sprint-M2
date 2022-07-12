5280f0909428c4de3c707f9c1a831d384e369eab
mpfuent <mariapazfuent@gmail.com>
2022-07-11 20:33:49 -0400

    Se agrega section con función de contador dscto

--- assets/js/index.js
+++ assets/js/index.js
@@ -29,3 +29,27 @@ $(document).ready(function() {
   });
 });
 
+function contadorSale() {
+    var countDownDate = new Date("Jul 31, 2022 23:59:59").getTime();
+    
+    var x = setInterval(function() {
+    
+      var now = new Date().getTime();
+    
+      var distance = countDownDate - now;
+    
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+      document.getElementById("saleend").innerHTML = days + "d " + hours + "h "
+      + minutes + "m " + seconds + "s ";
+    
+      if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("saleend").innerHTML = "¡Promoción agotada!";
+      }
+    }, 1000);}
+
+    contadorSale();
