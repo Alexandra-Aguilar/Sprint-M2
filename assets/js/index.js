f8532355f30eff685edec231ef726cb057f0de3a
mpfuent <mariapazfuent@gmail.com>
2022-07-13 18:56:57 -0400

    Se edita indentación

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,27 @@
-function showDateAndTime(){
-    var today=new Date();  
-    document.getElementById('fecha').innerHTML = today.toLocaleString(); 
-    updateTime();
+function showDateAndTime() {
+  var today = new Date();
+  document.getElementById('fecha').innerHTML = today.toLocaleString();
+  updateTime();
 }
 
-function updateTime(){
-    var refresh = 1000;
-    time = setTimeout('showDateAndTime()', refresh);
+function updateTime() {
+  var refresh = 1000;
+  time = setTimeout('showDateAndTime()', refresh);
 }
 
 
 
 // jQuery para validacion de Email
 
-$(document).ready(function() {
-    $('#comprobar').click(function(){
-        if($("#exampleFormControlInput1").val().indexOf('@', 0) == -1 || $("#exampleFormControlInput1").val().indexOf('.', 0) == -1) {
-            alert('El correo electrónico introducido no es correcto.');
-            return false;
-        }
+$(document).ready(function () {
+  $('#comprobar').click(function () {
+    if ($("#exampleFormControlInput1").val().indexOf('@', 0) == -1 || $("#exampleFormControlInput1").val().indexOf('.', 0) == -1) {
+      alert('El correo electrónico introducido no es correcto.');
+      return false;
+    }
 
-        alert('El email introducido es correcto.');
-    });
+    alert('El email introducido es correcto.');
+  });
 
   $("#btnfecha").click(function () {
     $("#fecha").toggle("slow", showDateAndTime());
@@ -30,78 +30,79 @@ $(document).ready(function() {
 });
 
 function contadorSale() {
-    var countDownDate = new Date("Jul 31, 2022 23:59:59").getTime();
-    
-    var x = setInterval(function() {
-    
-      var now = new Date().getTime();
-    
-      var distance = countDownDate - now;
-    
-      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-      document.getElementById("saleend").innerHTML = days + "d " + hours + "h "
+  var countDownDate = new Date("Jul 31, 2022 23:59:59").getTime();
+
+  var x = setInterval(function () {
+
+    var now = new Date().getTime();
+
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("saleend").innerHTML = days + "d " + hours + "h "
       + minutes + "m " + seconds + "s ";
-    
-      if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("saleend").innerHTML = "¡Promoción agotada!";
-      }
-    }, 1000);}
 
-    contadorSale();
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("saleend").innerHTML = "¡Promoción agotada!";
+    }
+  }, 1000);
+}
+
+contadorSale();
 
 
-    // validacion de rut
+// validacion de rut
 
 function checkRut(rut) {
   // Despejar Puntos
-  var valor = rut.value.replace('.','');
+  var valor = rut.value.replace('.', '');
   // Despejar Guión
-  valor = valor.replace('-','');
-  
+  valor = valor.replace('-', '');
+
   // Aislar Cuerpo y Dígito Verificador
-  cuerpo = valor.slice(0,-1);
+  cuerpo = valor.slice(0, -1);
   dv = valor.slice(-1).toUpperCase();
-  
+
   // Formatear RUN
-  rut.value = cuerpo + '-'+ dv
-  
+  rut.value = cuerpo + '-' + dv
+
   // Si no cumple con el mínimo ej. (n.nnn.nnn)
-  if(cuerpo.length < 7) { rut.setCustomValidity("RUT Incompleto"); return false;}
-  
+  if (cuerpo.length < 7) { rut.setCustomValidity("RUT Incompleto"); return false; }
+
   // Calcular Dígito Verificador
   suma = 0;
   multiplo = 2;
-  
+
   // Para cada dígito del Cuerpo
-  for(i=1;i<=cuerpo.length;i++) {
-  
-      // Obtener su Producto con el Múltiplo Correspondiente
-      index = multiplo * valor.charAt(cuerpo.length - i);
-      
-      
-      // Sumar al Contador General
-      suma = suma + index;
-      
-      // Consolidar Múltiplo dentro del rango [2,7]
-      if(multiplo < 7) { multiplo = multiplo + 1; } else { multiplo = 2; }
+  for (i = 1; i <= cuerpo.length; i++) {
+
+    // Obtener su Producto con el Múltiplo Correspondiente
+    index = multiplo * valor.charAt(cuerpo.length - i);
+
+
+    // Sumar al Contador General
+    suma = suma + index;
+
+    // Consolidar Múltiplo dentro del rango [2,7]
+    if (multiplo < 7) { multiplo = multiplo + 1; } else { multiplo = 2; }
 
   }
-  
+
   // Calcular Dígito Verificador en base al Módulo 11
   dvEsperado = 11 - (suma % 11);
-  
+
   // Casos Especiales (0 y K)
-  dv = (dv == 'K')?10:dv;
-  dv = (dv == 0)?11:dv;
-  
+  dv = (dv == 'K') ? 10 : dv;
+  dv = (dv == 0) ? 11 : dv;
+
   // Validar que el Cuerpo coincide con su Dígito Verificador
-  if(dvEsperado != dv) { rut.setCustomValidity("RUT Inválido"); return false; }
-  
+  if (dvEsperado != dv) { rut.setCustomValidity("RUT Inválido"); return false; }
+
   // Si todo sale bien, eliminar errores (decretar que es válido)
   rut.setCustomValidity('');
 }
